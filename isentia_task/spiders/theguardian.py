662e3e3bbdf01e5e1dbaaf8ec3921c0c87a4689d
sonyfe25cp <sonyfe25cp@gmail.com>
2016-02-22 00:15:01 +0800

    update loggers and extract main content

--- isentia_task/spiders/theguardian.py
+++ isentia_task/spiders/theguardian.py
@@ -3,6 +3,9 @@ import scrapy
 import re
 from scrapy.selector import Selector
 from isentia_task.items import IsentiaTaskItem
+import time
+import html2text
+
 
 class TheguardianSpider(scrapy.Spider):
     
@@ -36,12 +39,16 @@ class TheguardianSpider(scrapy.Spider):
         #print response.url
         link = response.url
         item = IsentiaTaskItem()
-        item['title'] = sel.xpath('//*[@id="article"]/header/div[1]/div/div/h1').extract()
+        raw_title = sel.xpath('//*[@id="article"]/header/div[1]/div/div/h1/text()').extract()
+        item['title'] = combine_text(raw_title)
         item['link'] = link
-        item['author'] = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[2]/p[1]/text()').extract()
-        item['content'] = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[3]').extract()
-        item['published_date'] = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[2]/p[2]//text()').extract()
-        item['crawled_date'] = ''
+        raw_author = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[2]/p[1]//text()').extract()
+        item['author'] = combine_text(raw_author)
+        raw_content = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[3]//text()').extract()
+        item['content'] = combine_text(raw_content)
+        raw_published_date = sel.xpath('//*[@id="article"]/div[2]/div/div[1]/div[2]/p[2]//text()').extract()
+        item['published_date'] = combine_text(raw_published_date[0:4])
+        item['crawled_date'] =  time.strftime('%Y-%m-%d',time.localtime(time.time()))
         m = re.search(r'http://www.theguardian.com/([\w-]+)/', link)
         if m :
             category = m.group(1)
@@ -49,10 +56,33 @@ class TheguardianSpider(scrapy.Spider):
         else :
             print 'not category link :', link
         print item
-        
-        
-        
+        return item
+    
+    
     #Self error control
     def error_back(self, response):
         self.logger.error('error in Request')
+        
+        
+def combine_text(text_list):
+    text = ''
+    for _text in text_list:
+        if _text != '\n':
+            text += _text
+    return text.replace('\n', '')
+
+#Remove html tags
+def clean_html(raw_html):
+    html = ''
+    if type(raw_html) == 'list':
+        for rh in raw_html:
+            if rh != '\n':
+                html += rh
+    else:
+        html = raw_html
+    text = ''
+    if html and len(html) > 0:
+        converter = html2text.HTML2Text()
+        text = converter.handle(html)
+    return text
         
