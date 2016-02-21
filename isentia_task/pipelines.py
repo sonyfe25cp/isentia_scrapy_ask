efb75fedf0a851184f6f96fdefe7a5e8b4d2e5be
sonyfe25cp <sonyfe25cp@gmail.com>
2016-02-22 00:13:27 +0800

    finish mongo pipeline

--- isentia_task/pipelines.py
+++ isentia_task/pipelines.py
@@ -6,6 +6,9 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymongo
+import logging
+
+logger = logging.getLogger(__name__)
 
 class IsentiaTaskPipeline(object):
     
@@ -14,7 +17,7 @@ class IsentiaTaskPipeline(object):
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
-    
+        
     def process_item(self, item, spider):
         return item
 
@@ -22,14 +25,16 @@ class IsentiaTaskPipeline(object):
     def from_crawler(cls, crawler): 
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
     
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri) 
         self.db = self.client[self.mongo_db]
+        logger.info('open mongo: %s and database: %s', self.mongo_uri, self.mongo_db)
     
     def close_spider(self, spider): 
+        logger.info('close mongo')
         self.client.close()
     
     def process_item(self, item, spider): 
