0dc592bb829ffd43d263c27fc0686f6835c606db
sonyfe25cp <sonyfe25cp@gmail.com>
2016-02-22 00:13:48 +0800

    update middlewares

---
+++ isentia_task/misc/middleware.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from proxy import PROXIES
+from agents import AGENTS
+
+import random
+import logging
+
+logger = logging.getLogger(__name__)
+
+class CustomHttpProxyMiddleware(object):
+
+    def process_request(self, request, spider):
+        if self.use_proxy(request):
+            p = random.choice(PROXIES)
+            try:
+                request.meta['proxy'] = "http://%s" % p['ip_port']
+            except Exception, e:
+                logger.error('error in Request is %s', e)
+
+    def use_proxy(self, request):
+        """
+        using direct download for depth <= 2
+        using proxy with probability 0.3
+        """
+        return False
+#        if "depth" in request.meta and int(request.meta['depth']) <= 2:
+#            return False
+#        i = random.randint(1, 10)
+#        return i <= 2
+
+
+class CustomUserAgentMiddleware(object):
+    def process_request(self, request, spider):
+        agent = random.choice(AGENTS)
+        request.headers['User-Agent'] = agent
+        logger.debug('%s is used for %s', agent, request.url)
