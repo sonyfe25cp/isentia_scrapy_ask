d6c3ed1567b47465d3e457d34b9e339ca7d9c205
sonyfe25cp <sonyfe25cp@gmail.com>
2016-02-22 13:23:49 +0800

    update demo

--- client/demo.py
+++ client/demo.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from query import query
-items = query('China', 0, 5)
+from query import QueryAPI
+
+MONGO_URI = 'mongodb://isentia_chenjie:crawler_task@aws-us-east-1-portal.10.dblayer.com:10588,aws-us-east-1-portal.11.dblayer.com:27067'
+MONGO_DB = 'isentia'
+MONGO_COLLECTION = 'isentia_items'
+    
+api = QueryAPI(MONGO_URI, MONGO_DB, MONGO_COLLECTION)
+
+(total, items) = api.query('China', 0, 5)
 for item in items:
     print item['title']
